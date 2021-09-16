0dba81c2fcbe789401c6bed764f9bc2e42afefde
mihoku <49892705+mihoku@users.noreply.github.com>
2021-09-16 18:34:14 +0700

    Add files via upload

--- basic2.py
+++ basic2.py
@@ -18,6 +18,7 @@ DATA_PATH = PATH.joinpath("data-source").resolve()
 OTHER_PATH = PATH.joinpath("other-datasets").resolve()
 
 df = pd.read_csv(DATA_PATH.joinpath('debt maturity.csv'),low_memory=False, sep=";", header=0)
+df2 = pd.read_csv(DATA_PATH.joinpath('debt currency.csv'),low_memory=False, sep=";", header=0)
 x = df['Tahun']
 
 composition_of_public_debt = go.Figure()
@@ -50,7 +51,7 @@ composition_of_public_debt.add_annotation(x=2020, y=100,
             text="Projections--->",showarrow=False)
 
 composition_of_public_debt.update_layout(yaxis_range=(0, 140))
-composition_of_public_debt.update_layout(width=800, height=500)
+composition_of_public_debt.update_layout(height=500)
 composition_of_public_debt.update_layout(barmode='relative', 
                                   colorway=px.colors.qualitative.Vivid, 
                                   title=dict(
@@ -74,10 +75,65 @@ composition_of_public_debt.update_layout(barmode='relative',
                                       t=90,
                                       pad=2))
 
-BASIC2 = dbc.Container(
+composition_of_public_debt_by_currency = go.Figure()
+composition_of_public_debt_by_currency.add_trace(go.Scatter(
+    x=x, y=df2['Local'],
+    hoverinfo='x+y',
+    mode='lines',
+    line=dict(width=0.2, color='rgb(255, 140, 0)'),
+    stackgroup='one', # define stack group
+    name='Local Currency'
+))
+composition_of_public_debt_by_currency.add_trace(go.Scatter(
+    x=x, y=df2['Foreign'],
+    hoverinfo='x+y',
+    mode='lines',
+    line=dict(width=0.2, color='rgb(0,128, 0)'),
+    stackgroup='one',
+    name='Foreign Currency'
+))
+
+
+composition_of_public_debt_by_currency.add_vrect(
+    x0="2021", x1="2026",
+    fillcolor="#888",
+    opacity=0.2, line_width=0
+)
+
+composition_of_public_debt_by_currency.add_annotation(x=2020, y=100,
+            text="Projections--->",showarrow=False)
+
+composition_of_public_debt_by_currency.update_layout(yaxis_range=(0, 140))
+composition_of_public_debt_by_currency.update_layout(height=500)
+composition_of_public_debt_by_currency.update_layout(barmode='relative', 
+                                  colorway=px.colors.qualitative.Vivid, 
+                                  title=dict(
+                                      text="Composition of Public Debt by Currency (in Percent of GDP)",
+                                      pad_t=30,
+                                      pad_b=50,
+                                      yanchor="top",
+                                      y=1
+                                      ),
+                                  legend=dict(
+                                      orientation="h",
+                                      yanchor="bottom",
+                                      y=1.02,
+                                      xanchor="right",
+                                      x=1
+                                      ),
+                                  margin=dict(
+                                      l=30,
+                                      r=30,
+                                      b=50,
+                                      t=90,
+                                      pad=2))
+
+BASIC2 = dbc.Container(dbc.Card(
     [
-        dbc.Row([dbc.Col(dcc.Graph(figure = composition_of_public_debt), md=12),], style={"marginTop": 30}),
-        dbc.Row([dbc.Col(dbc.Card()),], style={"marginTop": 30}),
-    ],
-    className="mt-12",
+     dbc.CardHeader(html.Center(html.H4("Composition of Public Debt"))),
+     dbc.CardBody([
+         dbc.Row([dbc.Col(dcc.Graph(figure = composition_of_public_debt), md=6),dbc.Col(dcc.Graph(figure = composition_of_public_debt_by_currency), md=6)], style={"marginTop": 30}),
+         dbc.Row([dbc.Col(dbc.Card()),], style={"marginTop": 30}),])
+    ]),
+    className="mt-12", style={"marginTop": 50,"marginBottom": 50}
 )
